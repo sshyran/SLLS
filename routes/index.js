4e013f7cfa13cba5bb1a9cbdd97d0ee7fe1fd32b
Simon Shi <simonsmh@gmail.com>
2019-04-01 21:41:36 +0800

    Switch to ejs

--- routes/index.js
+++ routes/index.js
@@ -5,7 +5,7 @@ var router = express.Router();
 /* GET home page. */
 router.get('/', function (req, res, next) {
   var result = JSON.parse(fs.readFileSync(__dirname + '/../db.json'));
-  res.render('index', { title: 'SLLS' , status: result});
+  res.render('index', { title: 'SLLS', status: result });
 });
 
 module.exports = router;
